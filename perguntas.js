d0fcc2533cde59bbbbecb011771516faabff2b8a
Murill.sousac <136130909+Murillsousac@users.noreply.github.com>
2024-10-18 07:48:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ordem paranormal' ,
+    'O que é o elemento de conhecimento?' ,
+    'O melhor elemento, por que é a afinidade do gal'
+)
+
+criaCartao(
+    'Naruto',
+    'Quem controla os 6 caminhos de Pain?',
+    'Nagato'
+)
+
+criaCartao(
+    'Jujutsu kaisen',
+    'Como funciona a expansão de dominio do itadori?',
+    'Nem o Gege akutami sabe'
+)
+
+criaCartao(
+    'Baldur gates',
+    'Qual é o melhor ataque da ShadowHeart?',
+    'Ignis(nunca errou)'
+)
